646579be82c6f3c11c67676a822196b07fb5db24
didwldn3032 <didwldn3032@naver.com>
2020-08-05 20:23:23 +0900

    two

--- shop/urls.py
+++ shop/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main),
-    path('shopping/', include('shopping.urls')),
+    path('', include('shopping.urls')),
 ]
 
 
