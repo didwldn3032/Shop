457d63931e8341f05a00554334c6b7d1d2ab80c8
didwldn3032 <didwldn3032@naver.com>
2020-08-05 20:28:41 +0900

    three

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name='shopping'
 urlpatterns = [
-    path('', views.open),
-    path('food', views.food),
-    path('fashion', views.fashion),
-    path('beauty', views.beauty),
+    path('', views.open, name="open"),
+    path('food', views.food, name="food"),
+    path('fashion', views.fashion, name="fashion"),
+    path('beauty', views.beauty, name="beauty"),
 ]
